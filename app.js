748977d0634677bb4105a8672cce2e72ff0f1053
Akash_Shinde <akshinde023@gmail.com>
2025-05-08 11:26:37 +0530

    Added more questions

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
 const arr = [
   {question: "HTML stands for?",
-    answer : [ "HYPERTEXT MARKUP LANGUAGE",
-     "HYPERTEXT LANGUAGE",
-     "HYPERLOOP MARKUP LANGUAGE",
-     "HIGH MARKUP LANGUAGE"],
-     correct: "HYPERTEXT MARKUP LANGUAGE"
-    },
-
-    {question: "CSS stands for?",
-    answer : [ "CLEAR STYLE SHEETS", "CASCADING STYLE SHEETS", "CONSOLE STYLE SHEETS", "CURRENT STYLE SHEETS"],
-     correct: "CASCADING STYLE SHEETS"
+   answer: [ "HYPERTEXT MARKUP LANGUAGE",
+            "HYPERTEXT LANGUAGE",
+            "HYPERLOOP MARKUP LANGUAGE",
+            "HIGH MARKUP LANGUAGE"],
+   correct: "HYPERTEXT MARKUP LANGUAGE"
   },
 
-    {question: "JS stands for?",
-    answer : [ "Java", "J Script", "JavaScript", "None of the above"],
-    correct : "JavaScript"
-  }
+  {question: "CSS stands for?",
+   answer: [ "CLEAR STYLE SHEETS", "CASCADING STYLE SHEETS", "CONSOLE STYLE SHEETS", "CURRENT STYLE SHEETS"],
+   correct: "CASCADING STYLE SHEETS"
+  },
 
-]
+  {question: "JS stands for?",
+   answer: [ "Java", "J Script", "JavaScript", "None of the above"],
+   correct: "JavaScript"
+  },
+
+  {question: "Which company developed JavaScript?",
+   answer: [ "Netscape", "Microsoft", "Google", "Oracle"],
+   correct: "Netscape"
+  },
+
+  {question: "Which of the following is a valid JavaScript data type?",
+   answer: [ "String", "Number", "Boolean", "All of the above"],
+   correct: "All of the above"
+  },
+
+  {question: "Which HTML element is used to define an internal style sheet?",
+    answer: [ "&lt;style&gt;", "&lt;script&gt;", "&lt;link&gt;", "&lt;css&gt;"],
+   correct: "&lt;style&gt;"
+  },
+
+  {question: "In CSS, what property is used to change the text color?",
+   answer: [ "color", "text-color", "font-color", "background-color"],
+   correct: "color"
+  },
+
+  {question: "Which HTML attribute is used to define inline styles?",
+   answer: [ "style", "class", "id", "href"],
+   correct: "style"
+  },
+
+  {question: "What is the correct syntax to link an external JavaScript file in an HTML document?",
+    answer: [ "&lt;script src='file.js'&gt;", "&lt;script href='file.js'&gt;", "&lt;js src='file.js'&gt;", "&lt;script link='file.js'&gt;"],
+    correct: "&lt;script src='file.js'&gt;"
+  },
+
+  {question: "Which of these is NOT a valid HTML element?",
+   answer: [ "&lt;div&gt;", "&lt;span&gt;", "&lt;block&gt;", "&lt;header&gt;"],
+   correct: "&lt;block&gt;"
+  }
+];
+
 
 let question = document.querySelector(".question");
 let answer = document.querySelector(".answer");
@@ -58,7 +93,7 @@ function displayQuestion(){
   else{
     result.style.display = "block"
     container.style.display = "none"
-    displayScore.innerHTML = `${score >= 2? "Congratulations!": "OH!"} You Scored ${score} out of ${arr.length}.` ;
+    displayScore.innerHTML = `${score >= 4? "Congratulations!": "OH!"} You Scored ${score} out of ${arr.length}.` ;
     clearInterval(interval)
   }
 }
